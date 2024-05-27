d177f609390908113153809e8835fd30ce4b93de
rare1k <hi@rare1k.dev>
2024-05-27 10:17:14 -0400

    feat: gray out dotfiles

--- index.ts
+++ index.ts
@@ -1,5 +1,15 @@
 import c from "colors/safe";
 import { resolve } from "path";
+import { readdir } from "node:fs/promises";
+
 // @ts-expect-error
 const path = resolve(process.argv.at(-1));
-console.log(path)
+const files = await readdir(path);
+for (let i = 0; i < files.length; i++) {
+    if (files[i].startsWith(".")) {
+        files[i] = c.gray(files[i]);
+    }
+    
+}
+
+console.log(files.join(" "))
