bf4aa5854d331d003c466278b3556c78a8b0a977
rare1k <hi@rare1k.dev>
2024-05-27 11:42:25 -0400

    feat: run prettier

--- index.ts
+++ index.ts
@@ -2,38 +2,37 @@ import c, { bgMagenta } from "colors/safe";
 import { resolve } from "path";
 import { readdir } from "node:fs/promises";
 
-
 let path = resolve(process.argv.at(-1) || "./");
-let files = await readdir("./",{ withFileTypes: true });
+let files = await readdir("./", { withFileTypes: true });
 try {
-    files = await readdir(path,{ withFileTypes: true });
+  files = await readdir(path, { withFileTypes: true });
+} catch (e) {
+  console.warn(c.red(c.bold("error: ")) + e);
 }
-catch (e) {
-    console.warn(c.red(c.bold("error: ")) + e)
-}
-let f:string[] = []
+let f: string[] = [];
 for (let i = 0; i < files.length; i++) {
-    f[i] = files[i].name
-    
-    if (files[i].isDirectory()) {
-        f[i] = c.blue(files[i].name)
-    }
-    
-    if (files[i].isSymbolicLink()) {
-        f[i] = c.red(files[i].name)
-    }
+  f[i] = files[i].name;
 
-    if ( files[i].name.startsWith(".")) {
-        f[i] = c.dim(files[i].name);
-    }
-    if (files[i].isDirectory() && files[i].name.startsWith(".")) {
-        f[i] = c.dim(c.yellow(files[i].name));
-    }
-    if (files[i].isBlockDevice()) {
-        f[i] = c.bgBlue(files[i].name)
-    }
-    if (files[i].isCharacterDevice()) {
-        f[i] = c.bold(files[i].name)
-    }}
+  if (files[i].isDirectory()) {
+    f[i] = c.blue(files[i].name);
+  }
 
-console.log(f.join("  "))
+  if (files[i].isSymbolicLink()) {
+    f[i] = c.red(files[i].name);
+  }
+
+  if (files[i].name.startsWith(".")) {
+    f[i] = c.dim(files[i].name);
+  }
+  if (files[i].isDirectory() && files[i].name.startsWith(".")) {
+    f[i] = c.dim(c.yellow(files[i].name));
+  }
+  if (files[i].isBlockDevice()) {
+    f[i] = c.bgBlue(files[i].name);
+  }
+  if (files[i].isCharacterDevice()) {
+    f[i] = c.bold(files[i].name);
+  }
+}
+
+console.log(f.join("  "));
